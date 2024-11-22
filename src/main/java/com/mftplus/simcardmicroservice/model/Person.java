fcf33a8762f5c80a465210828b1aa46de0b534a7
farhang <farhang.farhanpoor@gmail.com>
2024-11-22 12:04:38 +0330

    Entity Updated

--- src/main/java/com/mftplus/simcardmicroservice/model/Person.java
+++ src/main/java/com/mftplus/simcardmicroservice/model/Person.java
@@ -1,8 +1,6 @@
 package com.mftplus.simcardmicroservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,9 +11,23 @@ import lombok.experimental.SuperBuilder;
 @Setter
 @SuperBuilder
 
-@Entity
-@Table
+@Entity(name = "personEntity")
+@Table(name = "person_tbl")
 public class Person {
     @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "national_id")
+    private String nationalId;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
 }
