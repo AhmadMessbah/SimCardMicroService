76a260dbcc4cb629e1a4362fc5075115c4147135
farhang <farhang.farhanpoor@gmail.com>
2024-11-22 13:32:34 +0330

    Updated MyControllerAdvice

---
+++ src/main/java/com/mftplus/simcardmicroservice/controller/exception/MyControllerAdvice.java
@@ -0,0 +1,32 @@
+package com.mftplus.simcardmicroservice.controller.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+
+@ControllerAdvice
+public class MyControllerAdvice {
+    @ExceptionHandler
+    public ResponseEntity<String> exceptionHandler(Exception ex) {
+        return ResponseEntity
+                .status(404)
+                .body("خطای ناشناخته - تماس با ادمین !!!" + ex.getMessage());
+    }
+
+
+    @ExceptionHandler(HttpClientErrorException.NotFound.class)
+    public ResponseEntity<String> exceptionHandler1(Exception ex) {
+        return ResponseEntity
+                .status(404)
+                .body("صفحه درخواستی وجود ندارد !!!");
+    }
+
+    @ExceptionHandler(HttpServerErrorException.class)
+    public ResponseEntity<String> exceptionHandler2(Exception ex) {
+        return ResponseEntity
+                .status(500)
+                .body("خطای سرور");
+    }
+}
