5e4b85b3becdd9de392bf05be41f882fef701260
sajjadchakameh <smsc7680@gmail.com>
2024-11-22 13:55:53 +0330

    index.html updated Loggable UnCommented

--- src/main/java/com/mftplus/simcardmicroservice/config/Loggable.java
+++ src/main/java/com/mftplus/simcardmicroservice/config/Loggable.java
@@ -11,31 +11,31 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class Loggable {
-//    @Before("execution(* com.mftplus.spring05..*.*(..))")
-//    public void logBefore(JoinPoint joinPoint) {
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        authentication.getAuthorities()
-//        if (! user.role.permissions.contains("Save")){
-//            throw new PermissionDeniedDataAccessException();
-//        }
-//    }
+    @Before("execution(* com.mftplus.SimCardMicroService..*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        authentication.getAuthorities()
+        if (! user.role.permissions.contains("Save")){
+            throw new PermissionDeniedDataAccessException();
+        }
+    }
+//
+    @Around("execution(* com.mftplus.SimCardMicroService..*.*(..))")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+//        log.info();
+        System.out.println("+ Run : " + joinPoint.getSignature().getName());
+        System.out.println("\t" + "Arguments");
+        for (Object arg : joinPoint.getArgs()) {
+            System.out.println("\t" + arg);
+        }
 
-//    @Around("execution(* com.mftplus.spring05..*.*(..))")
-//    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
-////        log.info();
-//        System.out.println("+ Run : " + joinPoint.getSignature().getName());
-//        System.out.println("\t" + "Arguments");
-//        for (Object arg : joinPoint.getArgs()) {
-//            System.out.println("\t" + arg);
-//        }
-//
-//        Object result = joinPoint.proceed();
-//        System.out.println("- Done " + joinPoint.getSignature().getName());
-//        return result;
-//    }
-//
-//    @After("execution(* com.mftplus.spring05..*.*(..))")
-//    public void drawLine() {
-//        System.out.println("--------------------------------------------");
-//    }
+        Object result = joinPoint.proceed();
+        System.out.println("- Done " + joinPoint.getSignature().getName());
+        return result;
+    }
+
+    @After("execution(* com.mftplus.SimCardMicroService..*.*(..))")
+    public void drawLine() {
+        System.out.println("--------------------------------------------");
+    }
 }
