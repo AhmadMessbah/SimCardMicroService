72994d56b8f463f912ec9082da75fc23af018adc
farhang <farhang.farhanpoor@gmail.com>
2024-11-22 12:23:32 +0330

    Updated

--- src/main/java/com/mftplus/simcardmicroservice/model/Person.java
+++ src/main/java/com/mftplus/simcardmicroservice/model/Person.java
@@ -19,15 +19,18 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "first_name")
-    private String firstName;
+    @Column(name = "name")
+    private String Name;
 
-    @Column(name = "last_name")
-    private String lastName;
+    @Column(name = "family")
+    private String family;
 
     @Column(name = "national_id")
     private String nationalId;
 
     @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Column(name = "address")
+    private String address;
 }
