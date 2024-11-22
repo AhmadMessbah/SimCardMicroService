089e9ae6e0bb6a2bfafcd504748870364e816e87
messbah.a <messbah.a@gmail.com>
2024-11-22 12:56:47 +0330

    messbah.a updated

--- src/main/java/com/mftplus/simcardmicroservice/service/PersonService.java
+++ src/main/java/com/mftplus/simcardmicroservice/service/PersonService.java
@@ -2,12 +2,13 @@ package com.mftplus.simcardmicroservice.service;
 
 import com.mftplus.simcardmicroservice.model.Person;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(value="personClient", url = "http://192.168.39.38")
+@FeignClient(value = "personClient", url = "http://192.168.39.38")
 public interface PersonService {
 
     @PostMapping("/persons")
-    Person postPerson(@RequestBody Person person) ;
+    ResponseEntity<String> postPerson(@RequestBody Person person);
 }
