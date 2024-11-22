0a007fc6890ad18e4815ddc05202b563aaf6eaf6
messbah.a <messbah.a@gmail.com>
2024-11-22 11:13:31 +0330

    first upload

---
+++ src/main/java/com/mftplus/simcardmicroservice/SimCardMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mftplus.simcardmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimCardMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimCardMicroServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mftplus/simcardmicroservice/SimCardMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mftplus.simcardmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimCardMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
