29e01c69d6f6f3fc5678df52a3d9043cc765a7de
messbah.a <messbah.a@gmail.com>
2024-11-22 12:33:57 +0330

    messbah.a updated

--- src/main/java/com/mftplus/simcardmicroservice/SimCardMicroServiceApplication.java
+++ src/main/java/com/mftplus/simcardmicroservice/SimCardMicroServiceApplication.java
@@ -1,5 +1,8 @@
 package com.mftplus.simcardmicroservice;
 
+import com.mftplus.simcardmicroservice.model.Person;
+import com.mftplus.simcardmicroservice.model.SimCard;
+import com.mftplus.simcardmicroservice.service.SimCardService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,9 +10,22 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class SimCardMicroServiceApplication {
+    private static SimCardService service;
+
+    public SimCardMicroServiceApplication(SimCardService service) {
+        SimCardMicroServiceApplication.service = service;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimCardMicroServiceApplication.class, args);
+
+        SimCard simCard =
+                SimCard
+                        .builder()
+                        .person(Person.builder().Name("ali").family("alipour").nationalId("1234567890").build())
+                        .number("09178505323")
+                        .build();
+        service.save(simCard);
     }
 
 }
