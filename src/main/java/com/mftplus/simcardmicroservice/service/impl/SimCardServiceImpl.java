e2463dff56cb540ab1e184d0367216019a5681b1
messbah.a <messbah.a@gmail.com>
2024-11-22 12:30:02 +0330

    first upload

--- src/main/java/com/mftplus/simcardmicroservice/service/impl/SimCardServiceImpl.java
+++ src/main/java/com/mftplus/simcardmicroservice/service/impl/SimCardServiceImpl.java
@@ -2,24 +2,24 @@ package com.mftplus.simcardmicroservice.service.impl;
 
 import com.mftplus.simcardmicroservice.model.Person;
 import com.mftplus.simcardmicroservice.model.SimCard;
+import com.mftplus.simcardmicroservice.repository.SimCardRepository;
 import com.mftplus.simcardmicroservice.service.PersonService;
-import com.mftplus.simcardmicroservice.service.SimCardService;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SimCardServiceImpl {
-    private final SimcardRepository simcardRepository;
+    private final SimCardRepository simcardRepository;
     private final PersonService personService;
 
-    public SimCardServiceImpl(SimcardRepository simcardRepository, PersonService personService) {
+    public SimCardServiceImpl(SimCardRepository simcardRepository, PersonService personService) {
         this.simcardRepository = simcardRepository;
         this.personService = personService;
     }
 
     @Transactional
-    public void save(SimCard simCard){
-        Person person =personService.postPerson(simCard.getPerson());
+    public void save(SimCard simCard) {
+        Person person = personService.postPerson(simCard.getPerson());
         System.out.println(person);
         simcardRepository.save(simCard);
     }
