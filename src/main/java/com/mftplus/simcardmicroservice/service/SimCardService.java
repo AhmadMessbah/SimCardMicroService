23d4a6c168ee7d995f42914f19e230927aebfabb
messbah.a <messbah.a@gmail.com>
2024-11-22 11:24:40 +0330

    first upload

--- src/main/java/com/mftplus/simcardmicroservice/service/SimCardService.java
+++ src/main/java/com/mftplus/simcardmicroservice/service/SimCardService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SimCardService {
-    @Autowired
     private final PersonService personService;
 
     public SimCardService(PersonService personService) {
